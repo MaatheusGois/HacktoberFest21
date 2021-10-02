247377741f90b063f6fabb1c49ff51d5aeadced1
Gois <matheusgoislimasilva@gmail.com>
2021-10-02 12:49:57 -0300

    create my contribution

---
+++ contributions/matheus-gois.swift
@@ -0,0 +1,9 @@
+//
+ //  Hello word.swift
+ //  HacktoberFest
+ //
+ //  Created by Matheus Gois on 02/10/21.
+ //  Copyright © 2021 Matheus Gois. All rights reserved.
+ //
+ print("Olá, meu nome é Matheus Gois");
+ print("Eu me tornei programador porque eu amo criar coisas novas <3");
