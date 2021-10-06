48b60b0724e81117017d6ffd5c3a80d96c6d3bf4
JaydipMak <92005713+JaydipMak@users.noreply.github.com>
2021-10-06 09:24:49 +0530

    Update matheus-gois.swift

--- contributions/matheus-gois.swift
+++ contributions/matheus-gois.swift
@@ -5,5 +5,5 @@
  //  Created by Matheus Gois on 02/10/21.
  //  Copyright © 2021 Matheus Gois. All rights reserved.
  //
- print("Olá, meu nome é Matheus Gois");
- print("Eu me tornei programador porque eu amo criar coisas novas <3");
+ print("Olá, meu nome é Matheus Gois")
+ print("Eu me tornei programador porque eu amo criar coisas novas <3")
