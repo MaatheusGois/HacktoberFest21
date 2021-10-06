21b7b51a21569deff08d12861a58b11b630331ac
DishaJogi <92005404+DishaJogi@users.noreply.github.com>
2021-10-06 09:43:41 +0530

    Update gabriel-henrique.c

--- contributions/gabriel-henrique.c
+++ contributions/gabriel-henrique.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
-int main(void) {
+main() {
   printf("Hello HacktoberFest21!\n\n");
   printf("Meu nome é Gabriel Henrique.\n");
   printf("Descobri que programação era o caminho certo para mim quando me dei conta que adoro encontrar soluções para diversos tipos de problemas!\n");
 
-  return 0;
+  
 }
