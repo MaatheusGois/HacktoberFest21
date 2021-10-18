49fa3034493c53dfd763bc56fc2de577c3bfda1e
Anuj Rajput <anujrajput04@gmail.com>
2021-10-19 00:51:35 +0530

    Create anuj-rajput.swift
    Added my purpose

---
+++ contributions/anuj-rajput.swift
@@ -0,0 +1,4 @@
+import Foundation
+
+print("I became an Engineer and a Programmer because I love building things from scratch and love doing things myself.")
+print("Programming empowers me to do whatever I want to achieve in my life. 💯")
